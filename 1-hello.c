82d3b7be68532d7cdf5bd3b8d46d489ef3c9e517
Niraj Baniya <nbaniya314@gmail.com>
2024-11-11 21:36:24 +0545

    Lab-1

---
+++ 1-hello.c
@@ -0,0 +1,6 @@
+// Write a Program to print Hello word in c.
+#include<stdio.h>
+int main(){
+     printf("Hello World");
+    return 0;
+}
---
+++ 2-sum-of-two-number.c
@@ -0,0 +1,1 @@
+// Write a program to add two numbers (5&7) and display its sum.
